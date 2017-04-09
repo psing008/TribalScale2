1a4dba2114b9b1b34b44e6976f80945f642cca1b
Piyush Singh <psing007@ucr.edu>
2017-04-09 10:34:22 -0700

    Initial Commit

---
+++ tribalScale2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tribalScale2
+//
+//  Created by Piyush Singh on 4/9/17.
+//  Copyright © 2017 Piyush Singh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ tribalScale2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tribalScale2
+//
+//  Created by Piyush Singh on 4/9/17.
+//  Copyright © 2017 Piyush Singh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
